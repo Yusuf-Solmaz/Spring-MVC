04ddb75c99453b5531556d83bc91a96caf33e1b5
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-02-19 23:18:23 +0300

    Using InitBinder

--- src/com/spring/mvcdemo/controller/UserController.java
+++ src/com/spring/mvcdemo/controller/UserController.java
@@ -2,9 +2,12 @@ package com.spring.mvcdemo.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +17,13 @@ import com.spring.mvcdemo.entities.User;
 @RequestMapping("/user")
 public class UserController {
 
+	
+	
+	@InitBinder
+	public void InitBinder(WebDataBinder webDataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 	
 	@RequestMapping("/showUserForm")
 	public String showUserAdd(Model model) {
